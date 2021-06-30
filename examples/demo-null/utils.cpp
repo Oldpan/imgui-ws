4c4fe50bb431256531aa927e82be825b1800c795
guoyanzong <guoyanzong@zuoyebang.com>
2021-06-30 21:25:55 +0800

    add utils.cpp

---
+++ examples/demo-null/utils.cpp
@@ -0,0 +1,13 @@
+#include "utils.h"
+#include <opencv2/opencv.hpp>
+
+void simgPadding(Mat img, Mat &imgRes, int minEdgeLimit, int maxEdgeLimit) {
+
+    double resizeRatio = maxEdgeLimit * 1.0 / max(img.rows,img.cols);
+    resize(img,imgRes,Size(resizeRatio*img.cols,img.rows*resizeRatio));
+    colResizeRatio = resizeRatio;
+    rowResizeRatio = resizeRatio;
+    Mat imgEdgeLimit(minEdgeLimit,maxEdgeLimit,img.type(),Scalar(0,0,0));
+    imgRes.copyTo(imgEdgeLimit(Rect(0,0,imgRes.cols,imgRes.rows)));
+    imgRes = imgEdgeLimit.clone();
+}
---
+++ examples/demo-null/utils.h
@@ -0,0 +1,5 @@
+/*展示辅助函数*/
+
+#pragma once
+
+void simgPadding(Mat img, Mat &imgRes, int minEdgeLimit, int maxEdgeLimit);
